13fae97a376bc0d63418b56dccd190c0dcdf875a
James Lovallo <jlovallo@Nextep.local>
2019-03-22 15:54:16 -0400

    Added public access token

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,8 @@ module.exports = {
   modules: [
     [
       'storyblok-nuxt',
-      { accessToken: 'lQVIeSpv6rmkjm63jCBqVwtt', cacheProvider: 'memory' }
+      { accessToken: 'LzfbLRAF56JNFMkGiOxx0gtt', cacheProvider: 'memory' }
+      // { accessToken: 'lQVIeSpv6rmkjm63jCBqVwtt', cacheProvider: 'memory' }
     ],
     ['@nuxtjs/markdownit']
   ],
