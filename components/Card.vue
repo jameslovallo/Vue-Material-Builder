f65ad13ea49db322215768a9b274327a2032c10d
James Lovallo <jlovallo@Nextep.local>
2019-03-23 11:41:52 -0400

    .

---
+++ components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-editable="blok">
+    <v-card>
+      <v-img :src="blok.image" :aspect-ratio="blok.aspectratio"></v-img>
+      <v-card-title primary-title>
+        <component
+          :key="blok._uid"
+          v-for="blok in blok.cardcontent"
+          :blok="blok"
+          :is="blok.component | dashify"
+        ></component>
+      </v-card-title>
+      <v-card-actions>
+        <component
+          :key="blok._uid"
+          v-for="blok in blok.cardactions"
+          :blok="blok"
+          :is="blok.component | dashify"
+        ></component>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["blok"]
+};
+</script>
