550dd43d74b6d342a745f9281249c57051778407
James Lovallo <jlovallo@Nextep.local>
2019-03-28 15:12:59 -0400

    Added PWA support

--- components/Image.vue
+++ components/Image.vue
@@ -6,7 +6,7 @@
       :contain="blok.contain"
       :gradient="blok.gradient"
       :height="blok.height"
-      :lazy-src="blok.lazysrc"
+      :lazy-src="blok.src"
       :max-height="blok.maxheight"
       :max-width="blok.maxwidth"
       :min-height="blok.minheight"
--- components/Masonry.vue
+++ components/Masonry.vue
@@ -3,7 +3,7 @@
     <div :class="blok.helpers" :style="blok.style">
       <masonry
         :cols="{default: blok.default_rows, 599: blok.xs_rows, 959: blok.sm_rows, 1263: blok.md_rows, 1903: blok.lg_rows}"
-        :gutter="blok.gutter"
+        :gutter="10"
       >
         <component
           :key="blok._uid"
@@ -19,12 +19,21 @@
 
 <script>
 export default {
-  props: ["blok"]
+  props: ["blok"],
+  watch: {
+    question: function (blok) {
+      var gutter = blok.gutter;
+      return gutter
+    }
+  }
 };
 </script>
 
 <style>
-.masonry * * * * {
+.masonry-content {
   margin-bottom: 10px;
+}
+.masonry {
+  margin-bottom: -10px;
 }
 </style>
