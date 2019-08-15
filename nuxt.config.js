b262423812ae9bbde59074b8503c2b535ae5ebec
James Lovallo <james.lovallo@mrm-mccann.com>
2019-08-15 12:06:44 -0400

    Trying to fix this damn Netlify issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors';
 
-// Customer-Specific Settings
+// Site-Specific Settings
+
 const SiteName = 'Patrick on Parker';
 const SiteUrl = 'https://patrickonparker.com';
 const StoryblokToken = 'lQVIeSpv6rmkjm63jCBqVwtt';
