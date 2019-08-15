4b0422c013cd4cca8bbe02c28718a391e7b48196
James Lovallo <james.lovallo@mrm-mccann.com>
2019-08-15 14:34:50 -0400

    .

--- middleware/setCacheVersion.js
+++ middleware/setCacheVersion.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
-export default function ({ app, route, store, isDev }) {
-  let version = route.query._storyblok || isDev ? 'draft' : 'published'
-  let language = route.params.language || 'en'
+export default function({ app, route, store, isDev }) {
+  let version = route.query._storyblok || isDev ? 'draft' : 'published';
+  let language = route.params.language || 'en';
 
   if (typeof window !== 'undefined' && window.StoryblokToken) {
-    Vue.prototype.$storyapi.accessToken = window.StoryblokToken
+    Vue.prototype.$storyapi.accessToken = window.StoryblokToken;
   }
 
   if (!store.state.cacheVersion) {
-    store.dispatch('loadCacheVersion')
+    store.dispatch('loadCacheVersion');
   }
 
   if (!store.state.settings._uid) {
-    store.dispatch('loadSettings', { version: version })
+    store.dispatch('loadSettings', { version: version });
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
-import colors from 'vuetify/es5/util/colors';
+import axios from 'axios';
 
 // Site-Specific Settings
-
 const SiteName = process.env.SITE_NAME;
 const SiteUrl = process.env.SITE_URL;
 const StoryblokToken = process.env.STORYBLOK_TOKEN;
-const GoogleAnalyticsID = process.env.GOOGLE_ANALYTICS;
 const BingWebmasterID = process.env.BING_WEBMASTER;
-const ProgressBarColor = process.env.PROGRESS_BAR_COLOR;
-const Favicon = process.env.FAVICON;
-
-// Load Dependencies
-const axios = require('axios');
+const GoogleAnalyticsID = process.env.GOOGLE_ANALYTICS;
+const ProgressBarColor = process.env.PROGRESS_BAR_COLOR
+  ? process.env.PROGRESS_BAR_COLOR
+  : '#fff';
+const ThemePrimary = process.env.THEME_PRIMARY
+  ? process.env.THEME_PRIMARY
+  : '#2979FF';
+const DarkTheme = process.env.DARK_THEME ? process.env.DARK_THEME : false;
 
 module.exports = {
   mode: 'universal',
@@ -27,138 +28,138 @@ module.exports = {
     ],
     link: [
       // Favicon
-      { rel: 'icon', type: 'image/x-icon', href: Favicon }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Progress Bar
+  loading: { color: ProgressBarColor },
+
+  // Plugins
+  plugins: [
+    '~/plugins/components',
+    '~/plugins/filters',
+    '~/plugins/masonry',
+    { src: 'plugins/vue-typer.js', ssr: false },
+    { src: 'plugins/vue-carousel-3d.js', ssr: false },
+    { src: 'plugins/magicgrid.js', ssr: false }
+  ],
+
+  // Nuxt Modules
+  modules: [
+    [
+      'storyblok-nuxt',
+      { accessToken: StoryblokToken, cacheProvider: 'memory' }
     ],
-
-    // Progress Bar
-    loading: { color: ProgressBarColor },
-
-    // Plugins
-    plugins: [
-      '~/plugins/components',
-      '~/plugins/filters',
-      '~/plugins/masonry',
-      { src: 'plugins/vue-typer.js', ssr: false },
-      { src: 'plugins/vue-carousel-3d.js', ssr: false },
-      { src: 'plugins/magicgrid.js', ssr: false }
+    ['@nuxtjs/markdownit'],
+    [
+      '@nuxtjs/pwa',
+      {
+        meta: { appleStatusBarStyle: 'default', name: SiteName }
+      }
     ],
+    ['@nuxtjs/sitemap']
+  ],
 
-    // Nuxt Modules
-    modules: [
-      [
-        'storyblok-nuxt',
-        { accessToken: StoryblokToken, cacheProvider: 'memory' }
-      ],
-      ['@nuxtjs/markdownit'],
-      [
-        '@nuxtjs/pwa',
-        {
-          meta: { appleStatusBarStyle: 'default', name: SiteName }
+  devModules: ['@nuxtjs/vuetify'],
+
+  vuetify: {
+    customProperties: true,
+    defaultAssets: {
+      font: true,
+      icons: 'mdi'
+    },
+    theme: {
+      dark: DarkTheme,
+      themes: {
+        light: {
+          primary: ThemePrimary
         }
-      ],
-      ['@nuxtjs/sitemap']
-    ],
-
-    devModules: ['@nuxtjs/vuetify'],
-
-    vuetify: {
-      customProperties: true,
-      defaultAssets: {
-        font: true,
-        icons: 'mdi'
       },
-      theme: {
-        dark: false,
-        themes: {
-          light: {
-            primary: colors.blue.accent3
-          }
-        },
-        options: {
-          customProperties: true
+      options: {
+        customProperties: true
+      }
+    }
+  },
+
+  css: ['@/assets/style/style.scss'],
+
+  // Sitemap Config
+  sitemap: {
+    hostname: SiteUrl,
+    gzip: true,
+    exclude: ['/global']
+  },
+
+  // Google Analytics
+  googleAnalytics: {
+    id: GoogleAnalyticsID
+  },
+
+  // Markdown-It Config
+  markdownit: { injected: true },
+
+  // Router Middleware
+  router: {
+    middleware: 'setCacheVersion'
+  },
+
+  // Build Configuration
+  build: {
+    extractCSS: true,
+    // PostCSS Config
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        'postcss-responsive-type': {}
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
         }
       }
     },
 
-    css: ['@/assets/style/style.scss'],
+    // You can extend webpack config here
+    extend(config, ctx) {}
+  },
 
-    // Sitemap Config
-    sitemap: {
-      hostname: SiteUrl,
-      gzip: true,
-      exclude: ['/global', '/home', 'global', 'home']
-    },
+  // Generate Routes
+  generate: {
+    routes: function(callback) {
+      const version = 'published';
+      let cache_version = 0;
 
-    // Google Analytics
-    googleAnalytics: {
-      id: GoogleAnalyticsID
-    },
+      // Other routes that are not in Storyblok with their slug.
+      let routes = ['/', '/home'];
 
-    // Markdown-It Config
-    markdownit: { injected: true },
+      // Load space and receive latest cache version key to improve performance
+      axios
+        .get(
+          `https://api.storyblok.com/v1/cdn/spaces/me?token=${StoryblokToken}`
+        )
+        .then(space_res => {
+          // timestamp of latest publish
+          cache_version = space_res.data.space.version;
 
-    // Router Middleware
-    router: {
-      middleware: 'setCacheVersion'
-    },
-
-    // Build Configuration
-    build: {
-      extractCSS: true,
-      // PostCSS Config
-      postcss: {
-        // Add plugin names as key and arguments as value
-        // Install them before as dependencies with npm or yarn
-        plugins: {
-          // Disable a plugin by passing false as value
-          'postcss-responsive-type': {}
-        },
-        preset: {
-          // Change the postcss-preset-env settings
-          autoprefixer: {
-            grid: true
-          }
-        }
-      },
-
-      // You can extend webpack config here
-      extend(config, ctx) {}
-    },
-
-    // Generate Routes
-    generate: {
-      routes: function(callback) {
-        const version = 'published';
-        let cache_version = 0;
-
-        // Other routes that are not in Storyblok with their slug.
-        let routes = ['/', '/home'];
-
-        // Load space and receive latest cache version key to improve performance
-        axios
-          .get(
-            `https://api.storyblok.com/v1/cdn/spaces/me?token=${StoryblokToken}`
-          )
-          .then(space_res => {
-            // timestamp of latest publish
-            cache_version = space_res.data.space.version;
-
-            // Call for all Links using the Links API: https://www.storyblok.com/docs/Delivery-Api/Links
-            axios
-              .get(
-                `https://api.storyblok.com/v1/cdn/links?token=${StoryblokToken}&version=${version}&cv=${cache_version}`
-              )
-              .then(res => {
-                Object.keys(res.data.links).forEach(key => {
-                  if (res.data.links[key].slug != 'home') {
-                    routes.push('/' + res.data.links[key].slug);
-                  }
-                });
-
-                callback(null, routes);
+          // Call for all Links using the Links API: https://www.storyblok.com/docs/Delivery-Api/Links
+          axios
+            .get(
+              `https://api.storyblok.com/v1/cdn/links?token=${StoryblokToken}&version=${version}&cv=${cache_version}`
+            )
+            .then(res => {
+              Object.keys(res.data.links).forEach(key => {
+                if (res.data.links[key].slug != 'home') {
+                  routes.push('/' + res.data.links[key].slug);
+                }
               });
-          });
-      }
+
+              callback(null, routes);
+            });
+        });
     }
   }
 };
