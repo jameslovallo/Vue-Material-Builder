9c20f0330a8cfbd56efc3b7318ad47a054bd1546
James Lovallo <james.lovallo@mrm-mccann.com>
2019-08-22 09:51:03 -0400

    Added aria label to buttons

--- components/Button.vue
+++ components/Button.vue
@@ -10,6 +10,7 @@
       <v-btn
         v-editable="blok"
         v-on="on"
+        :aria-label="blok.text"
         :absolute="blok.absolute"
         :active-class="blok.active_class"
         :block="blok.block"
