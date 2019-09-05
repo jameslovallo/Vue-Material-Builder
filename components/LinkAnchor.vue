d7af66bcfb0483ee8e0e4692633583d052dcaccf
James Lovallo <james.lovallo@mrm-mccann.com>
2019-09-05 12:53:27 -0400

    Added dynamic hash changing!!!!!!!

--- components/LinkAnchor.vue
+++ components/LinkAnchor.vue
@@ -1,10 +1,26 @@
 <template>
-  <div :id="blok.id" class="anchor-target"></div>
+  <div :id="blok.id" class="anchor-target" v-scroll="onScroll"></div>
 </template>
 
 <script>
 export default {
-  props: ["blok"]
+  props: ["blok"],
+  methods: {
+    onScroll() {
+      if (process.client) {
+        var el = document.getElementById(this.blok.id);
+        var top = el.getBoundingClientRect().top;
+        if (top >= -200 && top <= 200) {
+          // this.$router.push("/#" + this.blok.id);
+          if (history.pushState) {
+            history.pushState(null, null, "/#" + this.blok.id);
+          } else {
+            location.hash = "/#" + this.blok.id;
+          }
+        }
+      }
+    }
+  }
 };
 </script>
 
