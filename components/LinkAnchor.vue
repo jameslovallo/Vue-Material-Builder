db6aabcdfdd56a48f14681ac6c2fbe6071f24696
James Lovallo <james.lovallo@mrm-mccann.com>
2019-09-05 10:40:43 -0400

    Added vertical adjustment to link targets

--- components/LinkAnchor.vue
+++ components/LinkAnchor.vue
@@ -1,9 +1,20 @@
 <template>
-	<div :id="blok.id"></div>
+  <div :id="blok.id" class="anchor-target"></div>
 </template>
 
 <script>
 export default {
-	props: ["blok"]
-}
+  props: ["blok"]
+};
 </script>
+
+<style lang="scss">
+.anchor-target {
+  position: relative;
+  top: -60px;
+  pointer-events: none;
+  @media (max-width: 959px) {
+    top: -56px;
+  }
+}
+</style>
