4eaa41f8405e77385b1b49d1659749034a9b5ab7
James Lovallo <james.lovallo@mrm-mccann.com>
2019-10-17 08:58:04 -0400

    Rolled back changes for more reliable loading

--- components/LinkAnchor.vue
+++ components/LinkAnchor.vue
@@ -1,34 +1,36 @@
 <template>
-  <div :id="blok.id" class="anchor-target" v-scroll="onScroll"></div>
+	<div :id="blok.id" class="anchor-target" v-scroll="onScroll"></div>
 </template>
 
 <script>
 export default {
-  props: ["blok"],
-  methods: {
-    onScroll() {
-      if (process.client) {
-        var el = document.getElementById(this.blok.id);
-        var top = el.getBoundingClientRect().top;
-        if (top >= -75 && top <= 75) {
-          this.$router.push("/#" + this.blok.id);
-        }
-        var toolbar = document.querySelector(".v-toolbar__content");
-        var selected = document.activeElement;
-        if (selected.parentNode == toolbar) selected.blur();
-      }
-    }
-  }
+	props: ["blok"],
+	methods: {
+		onScroll() {
+			if (process.client && this.blok.push_route === true) {
+				var el = document.getElementById(this.blok.id);
+				var top = el.getBoundingClientRect().top;
+				if (top >= -75 && top <= 75) {
+					window.location.hash = this.blok.id;
+				} else if (window.scrollY <= 0) {
+					window.location.hash = "";
+				}
+				var toolbar = document.querySelector(".v-toolbar__content");
+				var selected = document.activeElement;
+				if (selected.parentNode == toolbar) selected.blur();
+			}
+		}
+	}
 };
 </script>
 
 <style lang="scss">
 .anchor-target {
-  position: relative;
-  top: -60px;
-  pointer-events: none;
-  @media (max-width: 959px) {
-    top: -56px;
-  }
+	position: relative;
+	top: -60px;
+	pointer-events: none;
+	@media (max-width: 959px) {
+		top: -56px;
+	}
 }
 </style>
